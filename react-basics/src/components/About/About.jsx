d1161bf87d09f6396d5a6611df9cdf8150c1ede7
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-08-19 19:51:16 +0530

    19/08/2025

---
+++ react-basics/src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './About.css'
+
+export default function About() {
+  return (
+  <section className='main-section'>
+    <div className='about'>
+        <h1>Hello I'm Abhishek Tiwari</h1>
+        <h2>Full Stack developer</h2>
+        <p>I am pursuing BCA from SDJ International College, Palsana, and Full Stack Development from Red & White Skill Education, Dindoli, Surat, to brighten my future and fulfill my dreams.</p>
+    </div>
+    <div className='img'>
+        <img src="https://avatars.githubusercontent.com/u/196152480?s=400&u=9cb1e4281b6b91d598e4f104dbb2699b1c10958d&v=4" alt="" />
+    </div>
+  </section>
+  )
+}
---
+++ react-basics/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <h2 className='logo'>PORTFOLIO</h2>
+        <div className='detail'>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Project</a>
+            <a href="">Experiance</a>
+            <a href="">Contact</a>
+        </div>
+      
+    </div>
+  )
+}
