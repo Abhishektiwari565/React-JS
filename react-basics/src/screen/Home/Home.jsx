2cc6a43b8399b6ad12029933a50f0730ade56123
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-08-20 20:52:43 +0530

    20/08/2025

--- react-basics/src/screen/Home/Home.jsx
+++ react-basics/src/screen/Home/Home.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import About from '../../components/About/About'
+import axios from 'axios'
 
 export default function Home() {
+  const [githubData,setGithubData]=useState({})
+  // to call api one time.
+  useEffect(()=>{
+    fetchApi().then((data)=>setGithubData(data));
+  },[]);
+   const fetchApi=async()=>{
+      const res=await axios.get("https://api.github.com/users/abhishektiwari565");
+      return res.data;
+    }
   return (
     <div>
-      <About/>
+      <About user={githubData}/>
     </div>
   )
 }
