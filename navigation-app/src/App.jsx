e19ded6f012e1b1243c8f5a80a282ac49cc0df59
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-09-10 13:54:57 +0530

    ...

--- navigation-app/src/App.jsx
+++ navigation-app/src/App.jsx
@@ -9,7 +9,6 @@ import Contact from './components/Contact'
 export default function App() {
   return (
     <>
-
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Navbar</a>
