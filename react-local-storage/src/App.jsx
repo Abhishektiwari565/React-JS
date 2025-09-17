e43fdb7841d68886df76aa0588e61b7ce34d17b0
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-09-17 23:20:42 +0530

    17/09/2025

---
+++ react-local-storage/src/App.jsx
@@ -0,0 +1,18 @@
+import { Route,Routes } from "react-router-dom";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+<Routes>
+  <Route path="/" element={<Home/>}></Route>
+  <Route path="/" element={<Home/>}></Route>
+</Routes>
+    </>
+  )
+}
+
+export default App
