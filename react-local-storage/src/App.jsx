5ab9afb15aef7f7082b5b84dc246f539a09e64de
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-09-18 11:42:12 +0530

    ....

--- react-local-storage/src/App.jsx
+++ react-local-storage/src/App.jsx
@@ -1,5 +1,10 @@
 import { Route,Routes } from "react-router-dom";
 import './App.css'
+import PrivateRoute from "./components/PrivateRoute";
+import SignIn from "./components/SignIn";
+import Cart from "./components/Cart";
+import { useState } from "react";
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,10 +12,20 @@ function App() {
   return (
     <>
 
-<Routes>
-  <Route path="/" element={<Home/>}></Route>
-  <Route path="/" element={<Home/>}></Route>
-</Routes>
+   <Routes>
+        <Route path='/' element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>} />
+        <Route path='/signin' element={
+            <PrivateRoute>
+              <SignIn />
+            </PrivateRoute>
+          } />
+        <Route path='/cart' element={<PrivateRoute>
+          <Cart />
+        </PrivateRoute>} />
+      </Routes>
     </>
   )
 }
