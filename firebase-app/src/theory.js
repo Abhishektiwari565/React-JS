69d5fc66f6edd4a8b31ff6d7dfe2fc8eee2c2fe7
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-11-04 22:52:38 +0530

    ...

---
+++ firebase-app/src/theory.js
@@ -0,0 +1,20 @@
+// what is firebase
+// ready-made backend for react apps
+// this firebase prvide cloud (accessible everywhere) storage 
+// authenticaton ->it is provide by google
+// firestore-database ->it is store text and number
+// firebase-storage ->it is store images and video 
+
+// what is database
+// it is collection of data and store data in systematically.
+// there are two types of database
+
+// 1.Relational database
+// ->it store data in tabular form (rows and cols)
+// ->example:sql
+// ->it is used when data is static and formated
+
+// 2.Non-relational database
+// ->it store data in key:value pair and collection:document pair in database
+// ->it is used when data is dynamic.
+// ->firebase and json server is example 
