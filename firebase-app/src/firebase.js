f886e1e7bdb770721b8c81306e45714f58c0da0c
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-11-05 18:06:46 +0530

    ...

---
+++ firebase-app/src/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDs_71cZ48A4khk-SEcO17FEn3TBuCZMLg",
+  authDomain: "fir-app-11214.firebaseapp.com",
+  projectId: "fir-app-11214",
+  storageBucket: "fir-app-11214.firebasestorage.app",
+  messagingSenderId: "223201726011",
+  appId: "1:223201726011:web:1f686b1b4dae3e78d5561e",
+  measurementId: "G-9KJM7NR8LH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth=getAuth(app);
